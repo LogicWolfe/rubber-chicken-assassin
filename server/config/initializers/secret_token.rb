5041e5318dc33375c0d70b992f3a38b210efe06f
Nathan Baxter <nathan@logicaldecay.net>
2011-02-12 10:00:39 -0800

    Initial rails generated project

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assassin::Application.config.secret_token = '409579690b4f6ff533d2e6c699dacf932e0964b8125ff91afcb48cfbed72d70bd8e12165055e55e7cd7e6628872ff691dd797c2d154b2a19e17f3b84f3366cb7'
