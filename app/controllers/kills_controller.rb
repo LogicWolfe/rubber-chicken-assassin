f4ff47698c82091193766ad6705a753cbaf5597a
Tony <tony@tonyle.ca>
2011-02-13 15:49:20 -0700

    Updated kills controller with all data needed for obit

--- app/controllers/kills_controller.rb
+++ app/controllers/kills_controller.rb
@@ -1,6 +1,6 @@
 class KillsController < ApplicationController
   protect_from_forgery :except => :create
-  
+
   # GET /kills
   # GET /kills.xml
   def index
@@ -27,7 +27,7 @@ class KillsController < ApplicationController
   # GET /kills/new.xml
   def new
     @kill = Kill.new
-    
+
     @kill.killer_id = "583002418"
     @kill.victim_id = "511852582"
 
@@ -41,31 +41,44 @@ class KillsController < ApplicationController
   def edit
     @kill = Kill.find(params[:id])
   end
-  
+
   # POST /kills
   # POST /kills.xml
   def create
     @kill = Kill.new(params[:kill])
 
     @access_token = params[:access_token]
-    
+
     @assassin = get_facebook_data(@kill.killer_id, nil, @access_token)
     @victim = get_facebook_data(@kill.victim_id, nil, @access_token)
     @friends = get_facebook_data(@kill.victim_id, 'friends', @access_token)
 
     @body = get_facebook_data(@kill.victim_id, 'movies', @access_token)
-    if (@favorite_movie != nil)
-      @favorite_movie = @body["data"][0]["name]"]  
-    end    
+    if (@body["data"][0].length > 0)
+      @favorite_movie = 'DEFAULT'
+    else
+      @favorite_movie = @body["data"][0]["name"]
+    end
 
     @body = get_facebook_data(@kill.victim_id, 'music', @access_token)
-    if (@favorite_band != nil)
-      @favorite_band = @body["data"][0]["name]"]  
+    if (@body["data"][0].length > 0)
+      @favorite_band = 'DEFAULT'
+    else
+      @favorite_band = @body["data"][0]["name"]
     end
-    
+
     @body = get_facebook_data(@kill.victim_id, 'books', @access_token)
-    if (@favorite_book != nil)
-      @favorite_book = @body["data"][0]["name]"]  
+    if (@body["data"][0].length > 0)
+      @favorite_book = 'DEFAULT'
+    else
+      @favorite_book = @body["data"][0]["name"]
+    end
+
+    @body = get_facebook_data(@kil.victim_id, 'events', @access_token)
+    if (@body["data"][0].length > 0)
+      @recent_event = 'DEFAULT'
+    else
+      @recent_event = @body["data"][0]["name"]
     end
 
     @body = get_facebook_data(@kill.victim_id, 'feed', @access_token)
@@ -76,10 +89,18 @@ class KillsController < ApplicationController
       end
     end
 
+    # Location
+    @locations_list = get_locations(@kill.location, @access_token)
+    if (@locations_list["data"].length > 0)
+      @location = @locations_list["data"][0]["name"]
+    else
+      @location = "the scene of the crime"
+    end
+
     @kill.create_obituary(
       :first_name => @victim["first_name"],
       :last_name => @victim["last_name"],
-      :assassin_full_name => @assassin['name'],
+      :assassin_full_name => @assassin["name"],
       :assassin_photo => RestClient.get("https://graph.facebook.com/#{@kill.killer_id}/picture", :params => {
         :type => :large,
         :access_token => @access_token
@@ -90,7 +111,8 @@ class KillsController < ApplicationController
       :favorite_band => @favorite_band,
       :favorite_book => @favorite_book,
       :last_status_update => @last_status_update,
-      :location => "LOCATION"
+      :location => @location,
+      :attack_sequence => @kill.attack_sequence
     )
 
     if @kill.save
