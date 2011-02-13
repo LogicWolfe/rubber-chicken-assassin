3c16d0e999d9b020a6b8ce39fe09b9a65d0afdd5
Nathan Baxter <nathan@logicaldecay.net>
2011-02-12 18:03:37 -0800

    Starting to add dragonfly for image uploades/manipulation.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,14 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Assassin
   class Application < Rails::Application
+
+    config.middleware.insert_after 'Rack::Lock', 'Dragonfly::Middleware', :images, '/media'
+    config.middleware.insert_before 'Dragonfly::Middleware', 'Rack::Cache', {
+      :verbose     => true,
+      :metastore   => "file:#{Rails.root}/tmp/dragonfly/cache/meta",
+      :entitystore => "file:#{Rails.root}/tmp/dragonfly/cache/body"
+    }
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/deploy.rb
+++ config/deploy.rb
@@ -42,3 +42,12 @@ namespace :deploy do
     CMD
   end
 end
+
+namespace :dragonfly do
+  desc "Symlink the Rack::Cache files"
+  task :symlink, :roles => [:app] do
+    run "mkdir -p #{shared_path}/tmp/dragonfly && ln -nfs #{shared_path}/tmp/dragonfly #{release_path}/tmp/dragonfly"
+  end
+end
+
+after 'deploy:update_code', 'dragonfly:symlink'
