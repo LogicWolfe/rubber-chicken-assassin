118e12b45c57882ae9f4d17731e13e348ae5df4b
Nathan Baxter <nathan@logicaldecay.com>
2011-02-27 17:54:34 -0800

    Don't show a random number, just show 42 if we don't know the friend count.

--- app/models/obituary.rb
+++ app/models/obituary.rb
@@ -90,7 +90,7 @@ class Obituary < ActiveRecord::Base
   end
   
   def fb_friends_count
-    read_attribute(:fb_friends_count) || randomize((0..150).to_a)
+    read_attribute(:fb_friends_count) || 42
   end
 
   private
